2de790c001e8d47b639afefe9b3d2a017dbb3ac7
Johannes99999 <123671316+Johannes99999@users.noreply.github.com>
2023-02-19 21:09:29 -0500

    Add files via upload

---
+++ Taller2_Hamburguesas/src/consola/Aplicacion.java
@@ -0,0 +1,67 @@
+package consola;
+
+import java.util.Scanner;
+
+import processamiento.Restaurante;
+
+public class Aplicacion {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Restaurant restaurant = new Restaurant();
+
+    public static void main(String[] args) {
+        restaurant.loadMenu();
+        restaurant.loadCombos();
+        restaurant.loadIngredients();
+        int option;
+
+        do {
+            printMenu();
+            option = scanner.nextInt();
+            scanner.nextLine(); // consume the newline character after reading the integer input
+
+            switch (option) {
+                case 1:
+                    restaurant.showMenu();
+                    break;
+                case 2:
+                    restaurant.startNewOrder(getClientName(), getClientAddress());
+                    break;
+                case 3:
+                    restaurant.addItemToOrder();
+                    break;
+                case 4:
+                    restaurant.closeOrder();
+                    break;
+                case 5:
+                    restaurant.getOrderInfo();
+                    break;
+                case 6:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option. Try again.");
+            }
+        } while (option != 6);
+    }
+
+    private static String getNombreCliente() {
+        System.out.print("Enter client name: ");
+        return scanner.nextLine();
+    }
+
+    private static String getDireccionCliente() {
+        System.out.print("Enter client address: ");
+        return scanner.nextLine();
+    }
+
+    private static void printMenu() {
+        System.out.println("Welcome to the Hamburger Shop!");
+        System.out.println("Choose an option:");
+        System.out.println("1. Show the menu");
+        System.out.println("2. Start a new order");
+        System.out.println("3. Add an item to an order");
+        System.out.println("4. Close an order and save the invoice");
+        System.out.println("5. Consult the information of an order given its ID");
+        System.out.println("6. Exit");
+    }
+}
---
+++ Taller2_Hamburguesas/src/modelo/ProductoAjustado.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class ProductoAjustado implements Producto {
+
+	private ProductoMenu base;
+
+	//Constructores
+	public ProductoAjustado (ProductoMenu base)
+	{
+		this.base = base;
+	}
+	//HER MA DET LEGGES TIL HVORDAN MAN LEGGER TIL OG TREKKER FRA INGREDIENSER.
+
+	@Override
+	public int getPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generarTextoFactura() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
